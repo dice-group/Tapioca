5c1822e07e3bb57c51b22db45d9cef15c008c685
MichaelRoeder <michaderstudent@hotmail.de>
2015-08-21 14:59:47 +0200

    Added an abstract class that can be used to apply extractors to a dump file.

---
+++ tapioca.analyzer/src/main/java/org/aksw/simba/tapioca/analyzer/dump/AbstractDumpExtractorApplier.java
@@ -0,0 +1,46 @@
+package org.aksw.simba.tapioca.analyzer.dump;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.zip.GZIPInputStream;
+
+import org.aksw.simba.tapioca.extraction.Extractor;
+import org.aksw.simba.tapioca.extraction.RDF2ExtractionStreamer;
+import org.apache.commons.io.IOUtils;
+import org.apache.jena.riot.RDFLanguages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractDumpExtractorApplier {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractDumpExtractorApplier.class);
+
+    protected ExecutorService executor = null;
+
+    public AbstractDumpExtractorApplier(ExecutorService executor) {
+        this.executor = executor;
+    }
+
+    protected void extractFromDump(String dump, Extractor... extractors) {
+        RDF2ExtractionStreamer streamer;
+        if (executor != null) {
+            streamer = new RDF2ExtractionStreamer(executor);
+        } else {
+            streamer = new RDF2ExtractionStreamer();
+        }
+        InputStream fin = null;
+        try {
+            fin = new FileInputStream(dump);
+            if (dump.endsWith(".gz")) {
+                fin = new GZIPInputStream(fin);
+                dump = dump.substring(0, dump.length() - 3);
+            }
+            streamer.runExtraction(fin, "", RDFLanguages.resourceNameToLang(dump), extractors);
+        } catch (Exception e) {
+            LOGGER.error("Couldn't read dump file \"" + dump + "\". Ignoring this dump.", e);
+        } finally {
+            IOUtils.closeQuietly(fin);
+        }
+    }
+}
--- tapioca.analyzer/src/main/java/org/aksw/simba/tapioca/analyzer/dump/DumpFileAnalyzer.java
+++ tapioca.analyzer/src/main/java/org/aksw/simba/tapioca/analyzer/dump/DumpFileAnalyzer.java
@@ -1,18 +1,11 @@
 package org.aksw.simba.tapioca.analyzer.dump;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.concurrent.ExecutorService;
-import java.util.zip.GZIPInputStream;
 
 import org.aksw.simba.tapioca.data.vocabularies.EVOID;
 import org.aksw.simba.tapioca.data.vocabularies.VOID;
-import org.aksw.simba.tapioca.extraction.Extractor;
-import org.aksw.simba.tapioca.extraction.RDF2ExtractionStreamer;
 import org.aksw.simba.tapioca.extraction.voidex.SpecialClassExtractor;
 import org.aksw.simba.tapioca.extraction.voidex.VoidExtractor;
-import org.apache.commons.io.IOUtils;
-import org.apache.jena.riot.RDFLanguages;
 
 import com.carrotsearch.hppc.ObjectIntOpenHashMap;
 import com.hp.hpl.jena.rdf.model.Model;
@@ -21,46 +14,23 @@ import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.RDF;
 
-public class DumpFileAnalyzer {
-
-    protected ExecutorService executor = null;
+public class DumpFileAnalyzer extends AbstractDumpExtractorApplier {
 
     public DumpFileAnalyzer() {
+        super(null);
     }
 
     public DumpFileAnalyzer(ExecutorService executor) {
-        this.executor = executor;
+        super(executor);
     }
 
     public Model extractVoidInfo(String datsetUri, String... dumps) {
         VoidExtractor extractor = new VoidExtractor();
         SpecialClassExtractor sExtractor = new SpecialClassExtractor();
         for (int i = 0; i < dumps.length; ++i) {
-            analyzeDump(dumps[i], extractor, sExtractor);
+            extractFromDump(dumps[i], extractor, sExtractor);
         }
         return generateVoidModel(datsetUri, extractor, sExtractor);
-    }
-
-    protected void analyzeDump(String dump, Extractor... extractors) {
-        RDF2ExtractionStreamer streamer;
-        if (executor != null) {
-            streamer = new RDF2ExtractionStreamer(executor);
-        } else {
-            streamer = new RDF2ExtractionStreamer();
-        }
-        InputStream fin = null;
-        try {
-            fin = new FileInputStream(dump);
-            if (dump.endsWith(".gz")) {
-                fin = new GZIPInputStream(fin);
-                dump = dump.substring(0, dump.length() - 3);
-            }
-            streamer.runExtraction(fin, "", RDFLanguages.resourceNameToLang(dump), extractors);
-        } catch (Exception e) {
-
-        } finally {
-            IOUtils.closeQuietly(fin);
-        }
     }
 
     protected Model generateVoidModel(String datsetUri, VoidExtractor extractor, SpecialClassExtractor sExtractor) {
