a7a48ce841e59d1cc0eca31db4500e808a2aff69
MichaelRoeder <michaasresearcher@gmail.com>
2015-09-03 17:11:32 +0200

    Added a commons library for reading compressed or archived dumps.

--- tapioca.analyzer/src/main/java/org/aksw/simba/tapioca/analyzer/dump/AbstractDumpExtractorApplier.java
+++ tapioca.analyzer/src/main/java/org/aksw/simba/tapioca/analyzer/dump/AbstractDumpExtractorApplier.java
@@ -7,6 +7,9 @@ import java.util.zip.GZIPInputStream;
 
 import org.aksw.simba.tapioca.extraction.Extractor;
 import org.aksw.simba.tapioca.extraction.RDF2ExtractionStreamer;
+import org.apache.commons.compress.compressors.CompressorInputStream;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
+import org.apache.commons.compress.compressors.FileNameUtil;
 import org.apache.commons.io.IOUtils;
 import org.apache.jena.riot.RDFLanguages;
 import org.slf4j.Logger;
@@ -14,34 +17,54 @@ import org.slf4j.LoggerFactory;
 
 public abstract class AbstractDumpExtractorApplier {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractDumpExtractorApplier.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractDumpExtractorApplier.class);
 
-    protected ExecutorService executor = null;
+	private static final String FILE_ENDING_COMPRESSION_MAPPING[][] = new String[][] {
+			{ ".gz", CompressorStreamFactory.GZIP }, { ".bz2", CompressorStreamFactory.BZIP2 } };
 
-    public AbstractDumpExtractorApplier(ExecutorService executor) {
-        this.executor = executor;
-    }
+	protected ExecutorService executor = null;
+	protected CompressorStreamFactory compStreamFactory = new CompressorStreamFactory();
 
-    protected boolean extractFromDump(String dump, Extractor... extractors) {
-        RDF2ExtractionStreamer streamer;
-        if (executor != null) {
-            streamer = new RDF2ExtractionStreamer(executor);
-        } else {
-            streamer = new RDF2ExtractionStreamer();
-        }
-        InputStream fin = null;
-        try {
-            fin = new FileInputStream(dump);
-            if (dump.endsWith(".gz")) {
-                fin = new GZIPInputStream(fin);
-                dump = dump.substring(0, dump.length() - 3);
-            }
-            return streamer.runExtraction(fin, "", RDFLanguages.resourceNameToLang(dump), extractors);
-        } catch (Exception e) {
-            LOGGER.error("Couldn't read dump file \"" + dump + "\". Ignoring this dump.", e);
-            return false;
-        } finally {
-            IOUtils.closeQuietly(fin);
-        }
-    }
+	public AbstractDumpExtractorApplier(ExecutorService executor) {
+		this.executor = executor;
+	}
+
+	protected boolean extractFromDump(String dump, Extractor... extractors) {
+		RDF2ExtractionStreamer streamer;
+		if (executor != null) {
+			streamer = new RDF2ExtractionStreamer(executor);
+		} else {
+			streamer = new RDF2ExtractionStreamer();
+		}
+		InputStream in = null;
+		try {
+			in = new FileInputStream(dump);
+			int compressionMethod = getCompressionMethod(dump);
+			while (compressionMethod >= 0) {
+				in = compStreamFactory.createCompressorInputStream(
+						FILE_ENDING_COMPRESSION_MAPPING[compressionMethod][1], in);
+				dump = dump.substring(0, dump.length() - FILE_ENDING_COMPRESSION_MAPPING[compressionMethod][0].length());
+				compressionMethod = getCompressionMethod(dump);
+			}
+			// if (dump.endsWith(".gz")) {
+			// in = new GZIPInputStream(in);
+			// dump = dump.substring(0, dump.length() - 3);
+			// }
+			return streamer.runExtraction(in, "", RDFLanguages.resourceNameToLang(dump), extractors);
+		} catch (Exception e) {
+			LOGGER.error("Couldn't read dump file \"" + dump + "\". Ignoring this dump.", e);
+			return false;
+		} finally {
+			IOUtils.closeQuietly(in);
+		}
+	}
+
+	protected int getCompressionMethod(String fileName) {
+		for (int i = 0; i < FILE_ENDING_COMPRESSION_MAPPING.length; ++i) {
+			if (fileName.endsWith(FILE_ENDING_COMPRESSION_MAPPING[i][0])) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
