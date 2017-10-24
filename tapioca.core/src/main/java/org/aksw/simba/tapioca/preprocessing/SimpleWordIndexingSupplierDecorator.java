2023b9c2d8c4005e0545bd9bf5f32a82f56eb5d9
Micha <michael.roeder@uni-paderborn.de>
2017-10-24 17:52:14 +0200

    Updated license information.

--- tapioca.core/src/main/java/org/aksw/simba/tapioca/preprocessing/SimpleWordIndexingSupplierDecorator.java
+++ tapioca.core/src/main/java/org/aksw/simba/tapioca/preprocessing/SimpleWordIndexingSupplierDecorator.java
@@ -1,18 +1,19 @@
 /**
- * This file is part of tapioca.core.
+ * tapioca.core - ${project.description}
+ * Copyright © 2015 Data Science Group (DICE) (michael.roeder@uni-paderborn.de)
  *
- * tapioca.core is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * tapioca.core is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with tapioca.core.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.aksw.simba.tapioca.preprocessing;
 
